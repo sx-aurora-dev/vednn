d0722a419db29217fa687a05cfe9aaa8f797f82a
Yoshiyuki Ohno <y-ohno@ji.jp.nec.com>
2019-07-19 17:20:17 +0900

    update Convolution/BackwardData/direct_dil1_pad0_ker1_owU128.c

--- src/intrinsic/Convolution/BackwardData/direct_dil1_pad0_ker1_owU128.c
+++ src/intrinsic/Convolution/BackwardData/direct_dil1_pad0_ker1_owU128.c
@@ -447,6 +447,120 @@ static inline void c16(
 }
 
 
+static inline void c16p(
+    const float * restrict pGOut,
+    const float * restrict pKernel,
+    float * restrict const pGIn,
+    const int64_t gOutChannel,
+    const int64_t gOutWidth,
+    const int64_t gOutHeight,
+    const int64_t gInChannel,
+    const int64_t gInWidth,
+    const int64_t gInHeight,
+    const int64_t strideWidth,
+    const int64_t strideHeight,
+    const int64_t gInChannelGroup,
+    const int64_t gOutChannelGroup,
+    const int64_t gInGroupOffset,
+    const int64_t gOutGroupOffset,
+    const int64_t kernGroupOffset,
+    const int64_t n,
+    const int64_t c,
+    const int64_t nY,
+    const __vr vrij
+)
+{
+  const float *pInChannel = pGIn + gInGroupOffset + ((n * gInChannel + c) * gInHeight * gInWidth ) ;
+  {
+    // zero filling
+    const int64_t nPixs = 16 * gInHeight * gInWidth ;
+    _ve_lvl(VLEN) ;
+    __vr vrzero = _ve_vbrdu_vs_f32(0.f) ;
+    for(int64_t ij=0; ij<nPixs ; ij+=VLEN) {
+      const int64_t vl = nPixs - ij < VLEN ? nPixs - ij : VLEN ;
+      _ve_lvl(vl) ;
+      _ve_vstu_vss(vrzero, 4, (void*)(pInChannel+ij)) ;
+    }
+  }
+
+  for (int64_t y=0; y<gOutHeight; y+=nY)
+  {
+    const int64_t vl = gOutWidth * (gOutHeight - y < nY ? gOutHeight - y : nY) ;
+    const int64_t op = y * gOutWidth ;
+
+    _ve_lvl(vl) ;
+
+    __vr vrsum01 = _ve_pvbrd_vs_i64(0UL) ;
+    __vr vrsum23 = _ve_pvbrd_vs_i64(0UL) ;
+    __vr vrsum45 = _ve_pvbrd_vs_i64(0UL) ;
+    __vr vrsum67 = _ve_pvbrd_vs_i64(0UL) ;
+    __vr vrsum89 = _ve_pvbrd_vs_i64(0UL) ;
+    __vr vrsumAB = _ve_pvbrd_vs_i64(0UL) ;
+    __vr vrsumCD = _ve_pvbrd_vs_i64(0UL) ;
+    __vr vrsumEF = _ve_pvbrd_vs_i64(0UL) ;
+
+
+    for(int64_t k=0; k<gOutChannelGroup; k++) {
+      int64_t outIndex = gOutGroupOffset + (n * gOutChannel + k  ) *  gOutHeight * gOutWidth + op ;
+
+      const float *pKerValue_k0 = pKernel + kernGroupOffset + ((k  ) * gInChannelGroup + c) ;
+      const uint64_t *pKerValue_u64_k0 = (const uint64_t*) pKerValue_k0 ;
+
+      __vr vrgout_k0 = _ve_vldu_vss(4, pGOut+outIndex) ;
+
+      __vr vrgoutP_k0 = _ve_vshf_vvvs(vrgout_k0, vrgout_k0, VE_VSHUFFLE_YUZU) ;
+      vrsum01 = _ve_pvfmad_vvsv(vrsum01, pKerValue_u64_k0[0], vrgoutP_k0) ;
+      vrsum23 = _ve_pvfmad_vvsv(vrsum23, pKerValue_u64_k0[1], vrgoutP_k0) ;
+      vrsum45 = _ve_pvfmad_vvsv(vrsum45, pKerValue_u64_k0[2], vrgoutP_k0) ;
+      vrsum67 = _ve_pvfmad_vvsv(vrsum67, pKerValue_u64_k0[3], vrgoutP_k0) ;
+      vrsum89 = _ve_pvfmad_vvsv(vrsum89, pKerValue_u64_k0[4], vrgoutP_k0) ;
+      vrsumAB = _ve_pvfmad_vvsv(vrsumAB, pKerValue_u64_k0[5], vrgoutP_k0) ;
+      vrsumCD = _ve_pvfmad_vvsv(vrsumCD, pKerValue_u64_k0[6], vrgoutP_k0) ;
+      vrsumEF = _ve_pvfmad_vvsv(vrsumEF, pKerValue_u64_k0[7], vrgoutP_k0) ;
+    }
+
+    __vr vrpgin_c0 = _ve_vsfa_vvss(vrij, 2, (uint64_t)(pInChannel+y*strideHeight*gInWidth)) ;
+    __vr vrpgin_c1 = _ve_vaddul_vsv(  4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_c2 = _ve_vaddul_vsv(2*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_c3 = _ve_vaddul_vsv(3*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_c4 = _ve_vaddul_vsv(4*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_c5 = _ve_vaddul_vsv(5*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_c6 = _ve_vaddul_vsv(6*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_c7 = _ve_vaddul_vsv(7*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_c8 = _ve_vaddul_vsv(8*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_c9 = _ve_vaddul_vsv(9*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_cA = _ve_vaddul_vsv(10*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_cB = _ve_vaddul_vsv(11*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_cC = _ve_vaddul_vsv(12*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_cD = _ve_vaddul_vsv(13*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_cE = _ve_vaddul_vsv(14*4*gInHeight*gInWidth,vrpgin_c0) ;
+    __vr vrpgin_cF = _ve_vaddul_vsv(15*4*gInHeight*gInWidth,vrpgin_c0) ;
+
+    _ve_svob() ;
+
+    _ve_vsclot_vv(vrsum01, vrpgin_c0) ;
+    _ve_vscuot_vv(vrsum01, vrpgin_c1) ;
+    _ve_vsclot_vv(vrsum23, vrpgin_c2) ;
+    _ve_vscuot_vv(vrsum23, vrpgin_c3) ;
+    _ve_vsclot_vv(vrsum45, vrpgin_c4) ;
+    _ve_vscuot_vv(vrsum45, vrpgin_c5) ;
+    _ve_vsclot_vv(vrsum67, vrpgin_c6) ;
+    _ve_vscuot_vv(vrsum67, vrpgin_c7) ;
+    _ve_vsclot_vv(vrsum89, vrpgin_c8) ;
+    _ve_vscuot_vv(vrsum89, vrpgin_c9) ;
+    _ve_vsclot_vv(vrsumAB, vrpgin_cA) ;
+    _ve_vscuot_vv(vrsumAB, vrpgin_cB) ;
+    _ve_vsclot_vv(vrsumCD, vrpgin_cC) ;
+    _ve_vscuot_vv(vrsumCD, vrpgin_cD) ;
+    _ve_vsclot_vv(vrsumEF, vrpgin_cE) ;
+    _ve_vscuot_vv(vrsumEF, vrpgin_cF) ;
+  }
+
+  _ve_svob() ;
+}
+
+
+
 vednnError_t
 vednnConvolutionBackwardData_direct_dil1_pad0_ker1_owU128(
     const vednnTensorParam_t * restrict 	pParamGradOut,
@@ -473,8 +587,8 @@ vednnConvolutionBackwardData_direct_dil1_pad0_ker1_owU128(
   const int64_t strideHeight   = pParamConv->strideHeight;
 //  const int64_t padWidth       = pParamConv->padWidth;	// must be 0
 //  const int64_t padHeight      = pParamConv->padHeight;	// must be 0
-  const int64_t dilationWidth  = pParamConv->dilationWidth;
-  const int64_t dilationHeight = pParamConv->dilationHeight;
+//  const int64_t dilationWidth  = pParamConv->dilationWidth;	// must be 1
+//  const int64_t dilationHeight = pParamConv->dilationHeight;	// must be 1
 
   const int64_t gOutChannelGroup = gOutChannel  / group;
   const int64_t gInChannelGroup  = gInChannel / group;
@@ -494,6 +608,8 @@ vednnConvolutionBackwardData_direct_dil1_pad0_ker1_owU128(
     __vr vri   = _ve_vmulsl_vsv(strideHeight, vry) ;
     __vr vrj   = _ve_vmulsl_vsv(strideWidth,  vrx) ;
     __vr vrij = _ve_vaddul_vvv(vrj, _ve_vmulul_vsv(gInWidth, vri)) ;
+
+    const int64_t usePackedKernel = (((uint64_t)pKernel) & 0x07) == 0 && (gInChannelGroup & 0x01) == 0 ?  1 : 0  ;
 
     for (int64_t n=0; n<batch; n++) {
       for (int64_t g = 0; g < group; g++) {
@@ -553,14 +669,26 @@ vednnConvolutionBackwardData_direct_dil1_pad0_ker1_owU128(
 	  c+=8 ;
 	}
 	for (; c<gInChannelGroup; c+=16) {
-	  c16(pGOut, pKernel, pGIn,
-	     gOutChannel, gOutWidth, gOutHeight,
-             gInChannel, gInWidth, gInHeight,
-             strideWidth, strideHeight,
-	     gInChannelGroup, gOutChannelGroup,
-	     gInGroupOffset, gOutGroupOffset, kernGroupOffset,
-	     n, c,
-	     nY, vrij) ;
+	  if( usePackedKernel ) {
+	    c16p(pGOut, pKernel, pGIn,
+	       gOutChannel, gOutWidth, gOutHeight,
+	       gInChannel, gInWidth, gInHeight,
+	       strideWidth, strideHeight,
+	       gInChannelGroup, gOutChannelGroup,
+	       gInGroupOffset, gOutGroupOffset, kernGroupOffset,
+	       n, c,
+	       nY, vrij) ;
+	  }
+	  else {
+	    c16(pGOut, pKernel, pGIn,
+	       gOutChannel, gOutWidth, gOutHeight,
+	       gInChannel, gInWidth, gInHeight,
+	       strideWidth, strideHeight,
+	       gInChannelGroup, gOutChannelGroup,
+	       gInGroupOffset, gOutGroupOffset, kernGroupOffset,
+	       n, c,
+	       nY, vrij) ;
+	  }
 
 	} // gInChannel
       } // group
