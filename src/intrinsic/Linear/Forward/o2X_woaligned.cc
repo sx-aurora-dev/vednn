0f3c42d3b0fe7788366e0af4e076b3abbdd23d6e
Yoshiyuki Ohno <ohno.yoshiyuki@nec.com>
2020-02-10 18:04:53 +0900

    update Linear/Forward/o2X_woaligned

--- src/intrinsic/Linear/Forward/o2X_woaligned.cc
+++ src/intrinsic/Linear/Forward/o2X_woaligned.cc
@@ -20,177 +20,133 @@ static inline void func(
   for(int64_t o=0; o<outDim; o+=2*VLEN) {
     const int64_t vl = (outDim-o < 2*VLEN ? outDim - o : 2*VLEN) >> 1 ;
 
-    __vr vrsum_b0 = _vel_pvbrd_vsl(0UL, vl) ;
-    __vr vrsum_b1 = _vel_pvbrd_vsl(0UL, vl) ;
-    __vr vrsum_b2 = _vel_pvbrd_vsl(0UL, vl) ;
-    __vr vrsum_b3 = _vel_pvbrd_vsl(0UL, vl) ;
+
+    __vr vrsum[BATCH] ;
+#pragma clang loop unroll(full)
+    for(int64_t b=0; b<BATCH; b++) {
+      vrsum[b] = _vel_pvbrd_vsl(0UL, vl) ;
+    }
 
     int64_t i=0;
     if((inDim & 0x01)==1) {
 	__vr vrw_i0 = _vel_vld_vssl(8, pWeight+(i  )*outDim+o, vl) ;
-	const uint64_t i_b0_0 = _vel_pack_f32a(pIn+0*inDim+i) ;
-	const uint64_t i_b1_0 = _vel_pack_f32a(pIn+1*inDim+i) ;
-	const uint64_t i_b2_0 = _vel_pack_f32a(pIn+2*inDim+i) ;
-	const uint64_t i_b3_0 = _vel_pack_f32a(pIn+3*inDim+i) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_0, vrw_i0, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_0, vrw_i0, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_0, vrw_i0, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_0, vrw_i0, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+0) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i0, vl) ;
+	}
 
 	i+=1 ;
     }
     if(((inDim>>1) & 0x01)==1) {
 	__vr vrw_i0 = _vel_vld_vssl(8, pWeight+(i  )*outDim+o, vl) ;
-	const uint64_t i_b0_0 = _vel_pack_f32a(pIn+0*inDim+i) ;
-	const uint64_t i_b1_0 = _vel_pack_f32a(pIn+1*inDim+i) ;
-	const uint64_t i_b2_0 = _vel_pack_f32a(pIn+2*inDim+i) ;
-	const uint64_t i_b3_0 = _vel_pack_f32a(pIn+3*inDim+i) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_0, vrw_i0, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_0, vrw_i0, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_0, vrw_i0, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_0, vrw_i0, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+0) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i0, vl) ;
+	}
 
 	__vr vrw_i1 = _vel_vld_vssl(8, pWeight+(i+1)*outDim+o, vl) ;
-	const uint64_t i_b0_1 = _vel_pack_f32a(pIn+0*inDim+i+1) ;
-	const uint64_t i_b1_1 = _vel_pack_f32a(pIn+1*inDim+i+1) ;
-	const uint64_t i_b2_1 = _vel_pack_f32a(pIn+2*inDim+i+1) ;
-	const uint64_t i_b3_1 = _vel_pack_f32a(pIn+3*inDim+i+1) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_1, vrw_i1, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_1, vrw_i1, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_1, vrw_i1, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_1, vrw_i1, vl) ;
-
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+1) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i1, vl) ;
+	}
 	i+=2 ;
     }
     if(((inDim>>2) & 0x01)==1) {
 	__vr vrw_i0 = _vel_vld_vssl(8, pWeight+(i  )*outDim+o, vl) ;
-	const uint64_t i_b0_0 = _vel_pack_f32a(pIn+0*inDim+i) ;
-	const uint64_t i_b1_0 = _vel_pack_f32a(pIn+1*inDim+i) ;
-	const uint64_t i_b2_0 = _vel_pack_f32a(pIn+2*inDim+i) ;
-	const uint64_t i_b3_0 = _vel_pack_f32a(pIn+3*inDim+i) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_0, vrw_i0, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_0, vrw_i0, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_0, vrw_i0, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_0, vrw_i0, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+0) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i0, vl) ;
+	}
 
 	__vr vrw_i1 = _vel_vld_vssl(8, pWeight+(i+1)*outDim+o, vl) ;
-	const uint64_t i_b0_1 = _vel_pack_f32a(pIn+0*inDim+i+1) ;
-	const uint64_t i_b1_1 = _vel_pack_f32a(pIn+1*inDim+i+1) ;
-	const uint64_t i_b2_1 = _vel_pack_f32a(pIn+2*inDim+i+1) ;
-	const uint64_t i_b3_1 = _vel_pack_f32a(pIn+3*inDim+i+1) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_1, vrw_i1, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_1, vrw_i1, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_1, vrw_i1, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_1, vrw_i1, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+1) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i1, vl) ;
+	}
 
 	__vr vrw_i2 = _vel_vld_vssl(8, pWeight+(i+2)*outDim+o, vl) ;
-	const uint64_t i_b0_2 = _vel_pack_f32a(pIn+0*inDim+i+2) ;
-	const uint64_t i_b1_2 = _vel_pack_f32a(pIn+1*inDim+i+2) ;
-	const uint64_t i_b2_2 = _vel_pack_f32a(pIn+2*inDim+i+2) ;
-	const uint64_t i_b3_2 = _vel_pack_f32a(pIn+3*inDim+i+2) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_2, vrw_i2, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_2, vrw_i2, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_2, vrw_i2, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_2, vrw_i2, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+2) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i2, vl) ;
+	}
 
 	__vr vrw_i3 = _vel_vld_vssl(8, pWeight+(i+3)*outDim+o, vl) ;
-	const uint64_t i_b0_3 = _vel_pack_f32a(pIn+0*inDim+i+3) ;
-	const uint64_t i_b1_3 = _vel_pack_f32a(pIn+1*inDim+i+3) ;
-	const uint64_t i_b2_3 = _vel_pack_f32a(pIn+2*inDim+i+3) ;
-	const uint64_t i_b3_3 = _vel_pack_f32a(pIn+3*inDim+i+3) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_3, vrw_i3, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_3, vrw_i3, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_3, vrw_i3, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_3, vrw_i3, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+3) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i3, vl) ;
+	}
 
       i+=4 ;
     }
     for(; i<inDim; i+=8 ) {
 	__vr vrw_i0 = _vel_vld_vssl(8, pWeight+(i  )*outDim+o, vl) ;
-	const uint64_t i_b0_0 = _vel_pack_f32a(pIn+0*inDim+i) ;
-	const uint64_t i_b1_0 = _vel_pack_f32a(pIn+1*inDim+i) ;
-	const uint64_t i_b2_0 = _vel_pack_f32a(pIn+2*inDim+i) ;
-	const uint64_t i_b3_0 = _vel_pack_f32a(pIn+3*inDim+i) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_0, vrw_i0, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_0, vrw_i0, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_0, vrw_i0, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_0, vrw_i0, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+0) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i0, vl) ;
+	}
 
 	__vr vrw_i1 = _vel_vld_vssl(8, pWeight+(i+1)*outDim+o, vl) ;
-	const uint64_t i_b0_1 = _vel_pack_f32a(pIn+0*inDim+i+1) ;
-	const uint64_t i_b1_1 = _vel_pack_f32a(pIn+1*inDim+i+1) ;
-	const uint64_t i_b2_1 = _vel_pack_f32a(pIn+2*inDim+i+1) ;
-	const uint64_t i_b3_1 = _vel_pack_f32a(pIn+3*inDim+i+1) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_1, vrw_i1, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_1, vrw_i1, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_1, vrw_i1, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_1, vrw_i1, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+1) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i1, vl) ;
+	}
 
 	__vr vrw_i2 = _vel_vld_vssl(8, pWeight+(i+2)*outDim+o, vl) ;
-	const uint64_t i_b0_2 = _vel_pack_f32a(pIn+0*inDim+i+2) ;
-	const uint64_t i_b1_2 = _vel_pack_f32a(pIn+1*inDim+i+2) ;
-	const uint64_t i_b2_2 = _vel_pack_f32a(pIn+2*inDim+i+2) ;
-	const uint64_t i_b3_2 = _vel_pack_f32a(pIn+3*inDim+i+2) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_2, vrw_i2, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_2, vrw_i2, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_2, vrw_i2, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_2, vrw_i2, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+2) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i2, vl) ;
+	}
 
 	__vr vrw_i3 = _vel_vld_vssl(8, pWeight+(i+3)*outDim+o, vl) ;
-	const uint64_t i_b0_3 = _vel_pack_f32a(pIn+0*inDim+i+3) ;
-	const uint64_t i_b1_3 = _vel_pack_f32a(pIn+1*inDim+i+3) ;
-	const uint64_t i_b2_3 = _vel_pack_f32a(pIn+2*inDim+i+3) ;
-	const uint64_t i_b3_3 = _vel_pack_f32a(pIn+3*inDim+i+3) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_3, vrw_i3, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_3, vrw_i3, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_3, vrw_i3, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_3, vrw_i3, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+3) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i3, vl) ;
+	}
 
 	__vr vrw_i4 = _vel_vld_vssl(8, pWeight+(i+4)*outDim+o, vl) ;
-	const uint64_t i_b0_4 = _vel_pack_f32a(pIn+0*inDim+i+4) ;
-	const uint64_t i_b1_4 = _vel_pack_f32a(pIn+1*inDim+i+4) ;
-	const uint64_t i_b2_4 = _vel_pack_f32a(pIn+2*inDim+i+4) ;
-	const uint64_t i_b3_4 = _vel_pack_f32a(pIn+3*inDim+i+4) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_4, vrw_i4, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_4, vrw_i4, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_4, vrw_i4, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_4, vrw_i4, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+4) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i4, vl) ;
+	}
 
 	__vr vrw_i5 = _vel_vld_vssl(8, pWeight+(i+5)*outDim+o, vl) ;
-	const uint64_t i_b0_5 = _vel_pack_f32a(pIn+0*inDim+i+5) ;
-	const uint64_t i_b1_5 = _vel_pack_f32a(pIn+1*inDim+i+5) ;
-	const uint64_t i_b2_5 = _vel_pack_f32a(pIn+2*inDim+i+5) ;
-	const uint64_t i_b3_5 = _vel_pack_f32a(pIn+3*inDim+i+5) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_5, vrw_i5, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_5, vrw_i5, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_5, vrw_i5, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_5, vrw_i5, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+5) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i5, vl) ;
+	}
 
 	__vr vrw_i6 = _vel_vld_vssl(8, pWeight+(i+6)*outDim+o, vl) ;
-	const uint64_t i_b0_6 = _vel_pack_f32a(pIn+0*inDim+i+6) ;
-	const uint64_t i_b1_6 = _vel_pack_f32a(pIn+1*inDim+i+6) ;
-	const uint64_t i_b2_6 = _vel_pack_f32a(pIn+2*inDim+i+6) ;
-	const uint64_t i_b3_6 = _vel_pack_f32a(pIn+3*inDim+i+6) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_6, vrw_i6, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_6, vrw_i6, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_6, vrw_i6, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_6, vrw_i6, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+6) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i6, vl) ;
+	}
 
 	__vr vrw_i7 = _vel_vld_vssl(8, pWeight+(i+7)*outDim+o, vl) ;
-	const uint64_t i_b0_7 = _vel_pack_f32a(pIn+0*inDim+i+7) ;
-	const uint64_t i_b1_7 = _vel_pack_f32a(pIn+1*inDim+i+7) ;
-	const uint64_t i_b2_7 = _vel_pack_f32a(pIn+2*inDim+i+7) ;
-	const uint64_t i_b3_7 = _vel_pack_f32a(pIn+3*inDim+i+7) ;
-	if(BATCH >=1) vrsum_b0 = _vel_pvfmad_vvsvl(vrsum_b0, i_b0_7, vrw_i7, vl) ;
-	if(BATCH >=2) vrsum_b1 = _vel_pvfmad_vvsvl(vrsum_b1, i_b1_7, vrw_i7, vl) ;
-	if(BATCH >=3) vrsum_b2 = _vel_pvfmad_vvsvl(vrsum_b2, i_b2_7, vrw_i7, vl) ;
-	if(BATCH >=4) vrsum_b3 = _vel_pvfmad_vvsvl(vrsum_b3, i_b3_7, vrw_i7, vl) ;
+#pragma clang loop unroll(full)
+	for(int64_t b=0; b<BATCH; b++) {
+	  const uint64_t in = _vel_pack_f32a(pIn+b*inDim+i+7) ;
+	  vrsum[b] = _vel_pvfmad_vvsvl(vrsum[b], in, vrw_i7, vl) ;
+	}
     }
 
-    if(BATCH >=1) _vel_vst_vssl(vrsum_b0, 8, pOut+0*outDim+o, vl) ;
-    if(BATCH >=2) _vel_vst_vssl(vrsum_b1, 8, pOut+1*outDim+o, vl) ;
-    if(BATCH >=3) _vel_vst_vssl(vrsum_b2, 8, pOut+2*outDim+o, vl) ;
-    if(BATCH >=4) _vel_vst_vssl(vrsum_b3, 8, pOut+3*outDim+o, vl) ;
+#pragma clang loop unroll(full)
+    for(int64_t b=0; b<BATCH; b++) {
+      _vel_vst_vssl(vrsum[b], 8, pOut+b*outDim+o, vl) ;
+    }
   }
 }
 extern "C"
@@ -209,7 +165,7 @@ vednnError_t vednnLinearForward_o2X_woaligned(
   float * __restrict__ const pOut    = (float * __restrict__ const) pDataOut;
 
   int64_t n=0;
-  int64_t batchRemain = nBatch & 0x03 ;
+  int64_t batchRemain = nBatch & 0x07 ;
 
   switch( batchRemain ) {
   case 1 :
@@ -224,11 +180,27 @@ vednnError_t vednnLinearForward_o2X_woaligned(
     func<3>(inDim, outDim, pIn+n*inDim, pWeight, pOut+n*outDim) ;
     n+=3 ;
     break ;
+  case 4 :
+    func<4>(inDim, outDim, pIn+n*inDim, pWeight, pOut+n*outDim) ;
+    n+=4 ;
+    break ;
+  case 5 :
+    func<5>(inDim, outDim, pIn+n*inDim, pWeight, pOut+n*outDim) ;
+    n+=5 ;
+    break ;
+  case 6 :
+    func<6>(inDim, outDim, pIn+n*inDim, pWeight, pOut+n*outDim) ;
+    n+=6 ;
+    break ;
+  case 7 :
+    func<7>(inDim, outDim, pIn+n*inDim, pWeight, pOut+n*outDim) ;
+    n+=7 ;
+    break ;
   default :
     break ;
   }
-  for(; n<nBatch; n+=4) {
-    func<4>(inDim, outDim, pIn+n*inDim, pWeight, pOut+n*outDim) ;
+  for(; n<nBatch; n+=8) {
+    func<8>(inDim, outDim, pIn+n*inDim, pWeight, pOut+n*outDim) ;
   }
 
   return VEDNN_SUCCESS ;
